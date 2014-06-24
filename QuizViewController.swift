81321d1bf0f15ed781ac377c08616ceb33902505
Matthew Rieger <matthewrieger@Matthews-MacBook-Pro.local>
2014-06-23 21:37:37 -0500

    Initial Project Setup

---
+++ QuizViewController.swift
@@ -0,0 +1,40 @@
+//
+//  QuizViewController.swift
+//  Quiz
+//
+//  Created by Matthew Rieger on 6/23/14.
+//  Copyright (c) 2014 Matthew Rieger. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
